487b01bca1b00563f31bc2f76c43b57d295262f8
Lia <Lia@LAPTOP-I5QOJDLO>
2019-04-29 21:38:50 +0800

    4/29

--- myproject/src/com/practice/MathEnglish.java
+++ myproject/src/com/practice/MathEnglish.java
@@ -6,8 +6,11 @@ public class MathEnglish {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 		int math;
 		int english;
+		String Score = scanner.nextLine();
+		this.Score = Score;
 		
 		
 	}
